b482768e5e76b3bd3422255e634eeb9be9a0afda
OrejadePollo <jesusramosguzman926@gmail.com>
2026-09-20 16:28:58 -0600

    feat(sistema-biblioteca): Se añaden funciones prototipo

---
+++ sistema-biblioteca.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int Busqueda_Libro(char *nombrelibro);
+void Contar_Libros();
+int Eliminar_Libro(char *nombrelibro);
+
+int main(){
+  printf("Aquí comenzamos a desarrollar el sistema de la bibloteca\n");
+
+  return 0;
+}
